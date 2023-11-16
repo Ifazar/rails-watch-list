cba82184e0afb74b1ae96e7bd8ab973de855ff14
RAZAFIMANOHIHAJA Aurelien <aurelienrazafi@gmail.com>
2023-11-16 11:41:30 +0100

    association & validation done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :list, uniqueness: { scope: :movie, message: 'already exists' }
+  validates :comment, length: { minimum: 6 }
 end
